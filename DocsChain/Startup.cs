8fa6c3adacc3256dd4ecf7d145c6f2b990f6ea3d
Giulio Fronterotta <gifronte@microsoft.com>
2021-04-27 15:33:52 +0200

    Security Update

--- DocsChain/Startup.cs
+++ DocsChain/Startup.cs
@@ -26,15 +26,16 @@ namespace DocsChain
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+           
             services.AddSingleton<ICore, Core>();
             services.AddSingleton<INetworkManager, NetworkManager>();
             services.AddSingleton<IHostedService, MainLoop>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
